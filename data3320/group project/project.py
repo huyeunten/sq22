647b638cea2eab3c800b2e0b14a0ea43e649e3d8
huyeunten <uyeuntenhale@seattleu.edu>
2022-08-22 18:31:20 -0700

    quarter end

---
+++ data3320/group project/project.py
@@ -0,0 +1,15 @@
+location = 'C:\Users\huyeu\Downloads'
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pandas as pd
+import scipy.io as sio
+import seaborn as sns
+from scipy.optimize import curve_fit
+from scipy.signal import find_peaks
+import scipy.io as sio
+import json
+
+sns.set_style("whitegrid")
+
